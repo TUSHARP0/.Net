41455193a83f4ec27798a342ab7fad2b66f1cfd3
TUSHARP0 <130496086+TUSHARP0@users.noreply.github.com>
2023-07-11 16:58:12 +0530

    Update Program.cs

--- Employee_Management-System/EmployeeMng/Program.cs
+++ Employee_Management-System/EmployeeMng/Program.cs
@@ -3,7 +3,7 @@
 // See https://aka.ms/new-console-template for more information
 Console.WriteLine("Hello, World!");
 Manager mng= new Manager(500,5000,1,
-    "Manasi",20000,"manasi@gmail.com","Kolhapur");
+    "Tushar",20000,"tushar@gmail.com","Nashik");
 Console.WriteLine(mng);
 
 Console.WriteLine("Manager Computed Salary: ");
